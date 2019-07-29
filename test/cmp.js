8590816be6bc57a07fbc160985f5f04f11c7fa97
Jordan Harband <ljharb@gmail.com>
2019-07-29 11:53:03 -0700

    [Tests] add `npm run lint`

--- test/cmp.js
+++ test/cmp.js
@@ -2,85 +2,86 @@ var test = require('tape');
 var equal = require('../');
 
 test('equal', function (t) {
-    t.ok(equal(
-        { a : [ 2, 3 ], b : [ 4 ] },
-        { a : [ 2, 3 ], b : [ 4 ] }
-    ));
-    t.end();
+  t.ok(equal(
+    { a: [2, 3], b: [4] },
+    { a: [2, 3], b: [4] }
+  ));
+  t.end();
 });
 
 test('not equal', function (t) {
-    t.notOk(equal(
-        { x : 5, y : [6] },
-        { x : 5, y : 6 }
-    ));
-    t.end();
+  t.notOk(equal(
+    { x: 5, y: [6] },
+    { x: 5, y: 6 }
+  ));
+  t.end();
 });
 
 test('nested nulls', function (t) {
-    t.ok(equal([ null, null, null ], [ null, null, null ]));
-    t.end();
+  t.ok(equal([null, null, null], [null, null, null]));
+  t.end();
 });
 
 test('strict equal', function (t) {
-    t.notOk(equal(
-        [ { a: 3 }, { b: 4 } ],
-        [ { a: '3' }, { b: '4' } ],
-        { strict: true }
-    ));
-    t.end();
+  t.notOk(equal(
+    [{ a: 3 }, { b: 4 }],
+    [{ a: '3' }, { b: '4' }],
+    { strict: true }
+  ));
+  t.end();
 });
 
 test('non-objects', function (t) {
-    t.ok(equal(3, 3));
-    t.ok(equal('beep', 'beep'));
-    t.ok(equal('3', 3));
-    t.notOk(equal('3', 3, { strict: true }));
-    t.notOk(equal('3', [3]));
-    t.end();
+  t.ok(equal(3, 3));
+  t.ok(equal('beep', 'beep'));
+  t.ok(equal('3', 3));
+  t.notOk(equal('3', 3, { strict: true }));
+  t.notOk(equal('3', [3]));
+  t.end();
 });
 
 test('arguments class', function (t) {
-    function getArgs() {
-      return arguments;
-    }
-    t.ok(
-       equal(getArgs(1, 2, 3), getArgs(1, 2, 3)),
-      'compares arguments'
-    );
+  function getArgs() {
+    return arguments;
+  }
+  t.ok(
+    equal(getArgs(1, 2, 3), getArgs(1, 2, 3)),
+    'compares arguments'
+  );
 
-    t.notOk(
-       equal(getArgs(1, 2, 3), [1, 2, 3]),
-      'differentiates array and arguments'
-    );
+  t.notOk(
+    equal(getArgs(1, 2, 3), [1, 2, 3]),
+    'differentiates array and arguments'
+  );
 
-    t.notOk(
-      equal([1, 2, 3], getArgs(1, 2, 3)),
-      'differentiates arguments and array'
-    );
+  t.notOk(
+    equal([1, 2, 3], getArgs(1, 2, 3)),
+    'differentiates arguments and array'
+  );
 
-    t.end();
+  t.end();
 });
 
 test('dates', function (t) {
-    var d0 = new Date(1387585278000);
-    var d1 = new Date('Fri Dec 20 2013 16:21:18 GMT-0800 (PST)');
-    t.ok(equal(d0, d1));
-    t.end();
+  var d0 = new Date(1387585278000);
+  var d1 = new Date('Fri Dec 20 2013 16:21:18 GMT-0800 (PST)');
+  t.ok(equal(d0, d1));
+  t.end();
 });
 
 test('buffers', function (t) {
-    t.ok(equal(Buffer('xyz'), Buffer('xyz')));
-    t.end();
+  /* eslint no-buffer-constructor: 1, new-cap: 1 */
+  t.ok(equal(Buffer('xyz'), Buffer('xyz')));
+  t.end();
 });
 
 test('booleans and arrays', function (t) {
-    t.notOk(equal(true, []));
-    t.end();
-})
+  t.notOk(equal(true, []));
+  t.end();
+});
 
 test('null == undefined', function (t) {
-    t.ok(equal(null, undefined))
-    t.notOk(equal(null, undefined, { strict: true }))
-    t.end()
-})
+  t.ok(equal(null, undefined));
+  t.notOk(equal(null, undefined, { strict: true }));
+  t.end();
+});
