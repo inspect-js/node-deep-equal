5b3bf315b603f00bab49e78c05c075cdc0dfd506
Jordan Harband <ljharb@gmail.com>
2019-07-30 22:42:36 -0700

    [Refactor] move Date and RegExp tests into `objEquiv`

--- index.js
+++ index.js
@@ -12,19 +12,6 @@ function deepEqual(actual, expected, options) {
     return true;
   }
 
-  var actualIsRegex = isRegex(actual);
-  var expectedIsRegex = isRegex(expected);
-  if (actualIsRegex || expectedIsRegex) {
-    if (actualIsRegex !== expectedIsRegex) {
-      return false;
-    }
-    return actual.source === expected.source && flags(actual) === flags(expected);
-  }
-
-  if (actual instanceof Date && expected instanceof Date) {
-    return actual.getTime() === expected.getTime();
-  }
-
   // 7.3. Other pairs that do not both pass typeof value == 'object', equivalence is determined by ==.
   if (!actual || !expected || (typeof actual !== 'object' && typeof expected !== 'object')) {
     return opts.strict ? is(actual, expected) : actual == expected;
@@ -60,39 +47,38 @@ function isBuffer(x) {
 }
 
 function objEquiv(a, b, opts) {
-  /* eslint max-statements: [2, 40] */
+  /* eslint max-statements: [2, 50] */
   var i, key;
-  if (isUndefinedOrNull(a) || isUndefinedOrNull(b)) {
-    return false;
-  }
+  if (isUndefinedOrNull(a) || isUndefinedOrNull(b)) { return false; }
 
   // an identical 'prototype' property.
-  if (a.prototype !== b.prototype) {
-    return false;
+  if (a.prototype !== b.prototype) { return false; }
+
+  if (isArguments(a) !== isArguments(b)) { return false; }
+
+  var aIsRegex = isRegex(a);
+  var bIsRegex = isRegex(b);
+  if (aIsRegex !== bIsRegex) { return false; }
+  if (aIsRegex || bIsRegex) {
+    return a.source === b.source && flags(a) === flags(b);
   }
 
-  if (isArguments(a) !== isArguments(b)) {
-    return false;
+  if (a instanceof Date && b instanceof Date) {
+    return a.getTime() === b.getTime();
   }
 
   if (isBuffer(a)) {
     if (!isBuffer(b)) {
       return false;
     }
-    if (a.length !== b.length) {
-      return false;
-    }
+    if (a.length !== b.length) { return false; }
     for (i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        return false;
-      }
+      if (a[i] !== b[i]) { return false; }
     }
     return true;
   }
 
-  if (typeof a !== typeof b) {
-    return false;
-  }
+  if (typeof a !== typeof b) { return false; }
 
   try {
     var ka = objectKeys(a);
@@ -101,25 +87,19 @@ function objEquiv(a, b, opts) {
     return false;
   }
   // having the same number of owned properties (keys incorporates hasOwnProperty)
-  if (ka.length !== kb.length) {
-    return false;
-  }
+  if (ka.length !== kb.length) { return false; }
 
   // the same set of keys (although not necessarily the same order),
   ka.sort();
   kb.sort();
   // ~~~cheap key test
   for (i = ka.length - 1; i >= 0; i--) {
-    if (ka[i] != kb[i]) {
-      return false;
-    }
+    if (ka[i] != kb[i]) { return false; }
   }
   // equivalent values for every corresponding key, and ~~~possibly expensive deep test
   for (i = ka.length - 1; i >= 0; i--) {
     key = ka[i];
-    if (!deepEqual(a[key], b[key], opts)) {
-      return false;
-    }
+    if (!deepEqual(a[key], b[key], opts)) { return false; }
   }
 
   return true;
