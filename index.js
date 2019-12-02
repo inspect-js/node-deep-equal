983780aec338b8d909c3f6a5b1e0f4d7fe1d78b5
Jordan Harband <ljharb@gmail.com>
2019-12-01 23:54:30 -0800

    [Refactor] use `side-channel` for memoizing circular refs

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var whichBoxedPrimitive = require('which-boxed-primitive');
 var callBound = require('es-abstract/helpers/callBound');
 var whichCollection = require('which-collection');
 var getIterator = require('es-get-iterator');
+var getSideChannel = require('side-channel');
 
 var $getTime = callBound('Date.prototype.getTime');
 var gPO = Object.getPrototypeOf;
@@ -18,7 +19,7 @@ var $mapHas = callBound('Map.prototype.has', true);
 var $mapGet = callBound('Map.prototype.get', true);
 var $setHas = callBound('Set.prototype.has', true);
 
-function internalDeepEqual(actual, expected, options, memos) {
+function internalDeepEqual(actual, expected, options, channel) {
   var opts = options || {};
 
   // 7.1. All identical values are equivalent, as determined by ===.
@@ -46,20 +47,23 @@ function internalDeepEqual(actual, expected, options, memos) {
    * corresponding key, and an identical 'prototype' property. Note: this
    * accounts for both named and indexed properties on Arrays.
    */
-  // see https://github.com/nodejs/node/commit/d3aafd02efd3a403d646a3044adcf14e63a88d32 for memos inspiration
+  // see https://github.com/nodejs/node/commit/d3aafd02efd3a403d646a3044adcf14e63a88d32 for memos/channel inspiration
 
-  var actualIndex = memos.actual.indexOf(actual);
-  if (actualIndex !== -1) {
-    if (actualIndex === memos.expected.indexOf(expected)) {
+  var hasActual = channel.has(actual);
+  var hasExpected = channel.has(expected);
+  var sentinel;
+  if (hasActual && hasExpected) {
+    if (channel.get(actual) === channel.get(expected)) {
       return true;
     }
+  } else {
+    sentinel = {};
   }
-
-  memos.actual.push(actual);
-  memos.expected.push(expected);
+  if (!hasActual) { channel.set(actual, sentinel); }
+  if (!hasExpected) { channel.set(expected, sentinel); }
 
   // eslint-disable-next-line no-use-before-define
-  return objEquiv(actual, expected, opts, memos);
+  return objEquiv(actual, expected, opts, channel);
 }
 
 function isUndefinedOrNull(value) {
@@ -79,7 +83,7 @@ function isBuffer(x) {
   return true;
 }
 
-function objEquiv(a, b, opts, memos) {
+function objEquiv(a, b, opts, channel) {
   /* eslint max-statements: [2, 100], max-lines-per-function: [2, 120], max-depth: [2, 5] */
   var i, key;
 
@@ -153,7 +157,7 @@ function objEquiv(a, b, opts, memos) {
   // equivalent values for every corresponding key, and ~~~possibly expensive deep test
   for (i = ka.length - 1; i >= 0; i--) {
     key = ka[i];
-    if (!internalDeepEqual(a[key], b[key], opts, memos)) { return false; }
+    if (!internalDeepEqual(a[key], b[key], opts, channel)) { return false; }
   }
 
   var aCollection = whichCollection(a);
@@ -172,13 +176,13 @@ function objEquiv(a, b, opts, memos) {
       while ((resultA = iA.next()) && (resultB = iB.next()) && !resultA.done && !resultB.done) {
         if (!$mapHas(a, resultB.value[0]) || !$mapHas(b, resultA.value[0])) { return false; }
         if (resultA.value[0] === resultB.value[0]) { // optimization: keys are the same, no need to look up values
-          if (!internalDeepEqual(resultA.value[1], resultB.value[1], opts, memos)) { return false; }
+          if (!internalDeepEqual(resultA.value[1], resultB.value[1], opts, channel)) { return false; }
         } else {
           aWithBKey = $mapGet(a, resultB.value[0]);
           bWithAKey = $mapGet(b, resultA.value[0]);
           if (
-            !internalDeepEqual(resultA.value[1], bWithAKey, opts, memos)
-            || !internalDeepEqual(resultB.value[1], aWithBKey, opts, memos)
+            !internalDeepEqual(resultA.value[1], bWithAKey, opts, channel)
+            || !internalDeepEqual(resultB.value[1], aWithBKey, opts, channel)
           ) {
             return false;
           }
@@ -198,5 +202,5 @@ function objEquiv(a, b, opts, memos) {
 }
 
 module.exports = function deepEqual(a, b, opts) {
-  return internalDeepEqual(a, b, opts, { actual: [], expected: [] });
+  return internalDeepEqual(a, b, opts, getSideChannel());
 };
