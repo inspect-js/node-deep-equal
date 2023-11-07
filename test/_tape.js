e02cadb650ef9d75684c015a35d2c59e029d5174
Jordan Harband <ljharb@gmail.com>
2023-11-06 22:49:28 -0800

    [Tests] port tests from main; only diff should be true/falses

--- test/_tape.js
+++ test/_tape.js
@@ -1,13 +1,34 @@
+'use strict';
+
 var Test = require('tape/lib/test');
 var is = require('object-is');
-var equal = require('../');
+
+var deepEqual = require('../');
+var assert = require('../assert');
+
+var equal = process.env.ASSERT ? function assertDeepEqual(a, b, options) {
+  try {
+    if (options && options.strict) {
+      assert.deepStrictEqual(a, b);
+    } else {
+      assert.deepEqual(a, b);
+    }
+  } catch (e) {
+    return false;
+  }
+  return true;
+} : deepEqual;
 
 function equalReversed(t, a, b, isEqual, msg, isStrict, skipReversed) {
-  var actual = isStrict ? equal(a, b, { strict: true }) : equal(a, b);
+  var actual = isStrict
+    ? equal(a, b, { strict: true })
+    : equal(a, b);
   var suffix = isEqual ? ' are equal' : ' are not equal';
   t.equal(actual, !!isEqual, msg + suffix);
   if (typeof skipReversed === 'boolean' ? !skipReversed : !is(a, b)) {
-    var actualReverse = isStrict ? equal(b, a, { strict: true }) : equal(b, a);
+    var actualReverse = isStrict
+      ? equal(b, a, { strict: true })
+      : equal(b, a);
     t.equal(actualReverse, !!isEqual, msg + suffix + ' (reversed)');
   }
 }
